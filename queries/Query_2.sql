4d5dd1985ab2f694ad1901b92e65f77490b99d9d
DetGrey <anenovlar@gmail.com>
2025-12-03 12:17:33 +0100

    queries are deleted

--- queries/Query_2.sql
+++
@@ -1,7 +0,0 @@
-SELECT * FROM trip_destination where trip_destination_id = 13;
-
-SELECT * FROM conversation_participant WHERE conversation_id = 18;
-
-Call get_user_conversations(81);
-
-DELETE FROM conversation where conversation_id = 17;
